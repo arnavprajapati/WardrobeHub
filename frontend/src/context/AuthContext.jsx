3cb7faa4fe9c39a5c6a432a2147d33c559fbea04
Arnav <arnavprajapati3101@gmail.com>
2025-08-31 09:42:28 +0530

    update backend URL to production for deployment

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext = createContext()
 function AuthContext({ children }) {
-    let serverURL = "http://localhost:4000"
+    let serverURL = import.meta.env.VITE_SERVER_URL
 
     let value = {
         serverURL
